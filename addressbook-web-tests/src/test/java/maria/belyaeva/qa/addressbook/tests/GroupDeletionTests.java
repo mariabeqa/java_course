70f1bbc11de0ca6cf97db98a8b1254980b8e4c59
maria_belyaeva_qa <maria.belyaeva.qa@gmail.com>
2018-08-30 01:41:32 +0600

    Добавлена проверка списка групп в тесте для удаления групп

--- addressbook-web-tests/src/test/java/maria/belyaeva/qa/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/maria/belyaeva/qa/addressbook/tests/GroupDeletionTests.java
@@ -4,6 +4,7 @@ import maria.belyaeva.qa.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
@@ -19,6 +20,12 @@ public class GroupDeletionTests extends TestBase {
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() -1);
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before,after);
     }
 
 }
