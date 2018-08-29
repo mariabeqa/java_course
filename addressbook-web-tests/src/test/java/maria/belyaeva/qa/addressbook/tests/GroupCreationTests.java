5f16a42a1ca9b907f5f6559beca0a5397c25f67f
maria_belyaeva_qa <maria.belyaeva.qa@gmail.com>
2018-08-29 21:16:18 +0600

    Добавлена сортировка списков в тесте для создания и модификации контактов

--- addressbook-web-tests/src/test/java/maria/belyaeva/qa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/maria/belyaeva/qa/addressbook/tests/GroupCreationTests.java
@@ -13,21 +13,16 @@ public class GroupCreationTests extends TestBase {
         public void testGroupCreation() {
             app.getNavigationHelper().goToGroupPage();
             List<GroupData> before = app.getGroupHelper().getGroupList();
-            GroupData group = new GroupData("test4", null, null);
+            GroupData group = new GroupData("test5", null, null);
             app.getGroupHelper().createGroup(group);
             List<GroupData> after = app.getGroupHelper().getGroupList();
             Assert.assertEquals(after.size(), before.size() + 1);
-
-            int max=0;
-            for(GroupData g : after) {
-                if(group.getId()>max) {
-                    max = g.getId();
-                }
-            }
-
-            int max1 = after.stream().max((Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId();
-            group.setId(max1);
+//            int max1 = after.stream().max((Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId();
+//            group.setId(max1);
             before.add(group);
-            Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+            Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(),g2.getId());
+            before.sort(byId);
+            after.sort(byId);
+            Assert.assertEquals(before, after);
         }
 }
