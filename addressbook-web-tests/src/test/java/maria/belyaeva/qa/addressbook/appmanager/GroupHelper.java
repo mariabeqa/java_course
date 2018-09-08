cb79986539dc77d790e674bed96b0ba2b21f0ba3
maria_belyaeva_qa <maria.belyaeva.qa@gmail.com>
2018-09-08 22:18:00 +0600

    Добавлено кеширование для групп

--- addressbook-web-tests/src/test/java/maria/belyaeva/qa/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/maria/belyaeva/qa/addressbook/appmanager/GroupHelper.java
@@ -5,9 +5,7 @@ import maria.belyaeva.qa.addressbook.model.Groups;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class GroupHelper extends HelperBase {
 
@@ -57,21 +55,23 @@ public class GroupHelper extends HelperBase {
         initGroupCreation();
         fillGroupForm(group);
         submitGroupCreation();
+        groupCache = null;
         returnToGroupPage();
     }
-
 
     public void modify(GroupData group) {
         selectGroupById(group.getId());
         initGroupModification();
         fillGroupForm(group);
         saveGroupModification();
+        groupCache = null;
         returnToGroupPage();
     }
 
     public void deleteById(GroupData group) {
         selectGroupById(group.getId());
         deleteSelectedGroups();
+        groupCache = null;
         returnToGroupPage();
     }
 
@@ -83,15 +83,20 @@ public class GroupHelper extends HelperBase {
         return wd.findElements(By.name("selected[]")).size();
     }
 
+    private Groups groupCache = null;
+
     public Groups all() {
-        Groups groups = new Groups();
+        if(groupCache != null) {
+            return new Groups(groupCache);
+        }
+        groupCache = new Groups();
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         for(WebElement element : elements) {
             String name = element.getText();
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-            groups.add(new GroupData().withId(id).withName(name));
+            groupCache.add(new GroupData().withId(id).withName(name));
         }
-        return groups;
+        return groupCache;
     }
 }
 
