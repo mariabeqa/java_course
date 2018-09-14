94392e8c747a2d0099981ce6dd22d9ae609b1b29
maria_belyaeva_qa <maria.belyaeva.qa@gmail.com>
2018-09-15 00:58:22 +0600

    Реализован провайдер в формате json для теста создания контактов

--- addressbook-web-tests/src/test/java/maria/belyaeva/qa/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/maria/belyaeva/qa/addressbook/tests/ContactCreationTests.java
@@ -1,22 +1,42 @@
 package maria.belyaeva.qa.addressbook.tests;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import maria.belyaeva.qa.addressbook.model.ContactData;
 import maria.belyaeva.qa.addressbook.model.Contacts;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 
-import java.io.File;
+import java.io.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
-    @Test()
-    public void testContactCreation() {
+
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+        return contacts.stream().map((c)-> new Object[] {c}).collect(Collectors.toList()).iterator();
+    }
+
+
+    @Test(dataProvider = "validContactsFromJson")
+    public void testContactCreation(ContactData contact) {
         Contacts before = app.contact().all();
         app.contact().initiateContactCreation();
-        File photo = new File("src/test/resources/pic.jpg");
-        ContactData contact = new ContactData().withFirstName("New").withLastName("New").withGroup("test1").withPhoto(photo);
         app.contact().create(contact);
         app.goTo().homePage();
         Contacts after = app.contact().all();
