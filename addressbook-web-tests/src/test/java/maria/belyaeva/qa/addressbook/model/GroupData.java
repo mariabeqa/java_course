349d5b9972bec0423f9cb1c348f135d5a7a4d84b
maria_belyaeva_qa <maria.belyaeva.qa@gmail.com>
2018-09-16 15:34:03 +0600

    Получение объектов GroupData с помощью ORM Hibernate

--- addressbook-web-tests/src/test/java/maria/belyaeva/qa/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/maria/belyaeva/qa/addressbook/model/GroupData.java
@@ -2,17 +2,36 @@ package maria.belyaeva.qa.addressbook.model;
 
 
 import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+@XStreamAlias("group")
+@javax.persistence.Entity
+@javax.persistence.Table(name = "group_list")
 public class GroupData {
     @Expose
+    @Column(name = "group_name")
     private String name;
+
     @Expose
+    @Column(name = "group_header")
+    @Type(type = "text")
     private String header;
+
     @Expose
+    @Column(name = "group_footer")
+    @Type(type = "text")
     private String footer;
+
     @XStreamOmitField
-    private int id = Integer.MAX_VALUE;;
+    @Id
+    @Column(name = "group_id")
+    private int id = Integer.MAX_VALUE;
 
     @Override
     public boolean equals(Object o) {
