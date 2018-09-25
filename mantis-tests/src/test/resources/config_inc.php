51ca2ec2352a9ac894153392cb40a0e3cd668718
maria_belyaeva_qa <maria.belyaeva.qa@gmail.com>
2018-09-25 13:20:33 +0600

    Реализована передача конфиг файла на удаленный сервер по протоколу FTP

---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,12 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = 'root';
+
+$g_default_timezone       = 'Asia/Omsk';
+
+$g_crypto_master_salt     = 'Md4vsrS0aMMty3Sjl7UgdAwfHspNBHho/jN8la8mwp8=';
+
+$g_signup_use_captcha = OFF;
