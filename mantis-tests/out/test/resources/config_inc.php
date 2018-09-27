335322bf7c01af6ac22e0241b465c6e6500c0521
maria_belyaeva_qa <maria.belyaeva.qa@gmail.com>
2018-09-27 12:43:17 +0600

    Реализовано получение почты на James сервер

--- mantis-tests/out/test/resources/config_inc.php
+++ mantis-tests/out/test/resources/config_inc.php
@@ -13,4 +13,3 @@ $g_signup_use_captcha = OFF;
 $g_phpMailer_method = PHPMAILER_METHOD_SMTP;
 $g_smtp_host = 'localhost';
 $g_smtp_port = 2500;
-
--- mantis-tests/src/test/java/maria/belyaeva/qa/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/maria/belyaeva/qa/mantis/tests/RegistrationTests.java
@@ -1,11 +1,10 @@
 package maria.belyaeva.qa.mantis.tests;
 
 import maria.belyaeva.qa.model.MailMessage;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 
+import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.List;
 
@@ -13,32 +12,34 @@ import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase {
 
-    @BeforeMethod
+    //@BeforeMethod
     public void startMailServer() throws IOException, InterruptedException {
         app.mail().start();
     }
 
     @Test
-    public void testRegistration() throws InterruptedException, IOException {
-        String email = "test333test@gmail.com";
-        String user = "test333test";
+    public void testRegistration() throws InterruptedException, IOException, MessagingException {
+        long now = System.currentTimeMillis();
+        String email = String.format("user%s@localhost.localdomain", now);
+        String user = String.format("user%s", now);
         String password = "password";
+        app.james().createUser(user, password);
         app.registration().start(user, email);
-        List<MailMessage> mailMessages =  app.mail().waitForMail(2, 10000);
+        //List<MailMessage> mailMessages =  app.mail().waitForMail(2, 10000);
+        List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
         String confirmationLink = findConfirmationLink(mailMessages, email);
-        System.out.println("confirmation link " + confirmationLink);
         app.registration().finish(confirmationLink, password);
         assertTrue(app.newSession().login(user, password));
     }
 
     private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
-        MailMessage mailMessage = mailMessages.stream().filter((m)->m.to.equals("test333test@gmail.com")).findFirst().get();
+        MailMessage mailMessage = mailMessages.stream().filter((m)->m.to.equals(email)).findFirst().get();
         System.out.println("mailMessage " + mailMessage.text);
         VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
         return regex.getText(mailMessage.text);
     }
 
-    @AfterMethod(alwaysRun = true)
+    //@AfterMethod(alwaysRun = true)
     public void stopMailServer() {
         app.mail().stop();
     }
